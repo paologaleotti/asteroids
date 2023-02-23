b281b2f671ccb0d39b9a7bd1d32979103eb219d2
paologaleotti <paologaleotti2002@gmail.com>
2023-02-23 23:15:48 +0100

    [ui] added text component

---
+++ ui/Text.lua
@@ -0,0 +1,43 @@
+local engine = require("love")
+
+function Text(text, x, y, font_size, wrap_width, align, opacity)
+    font_size = font_size or "p"
+    wrap_width = wrap_width or engine.graphics.getWidth()
+    align = align or "left"
+    opacity = opacity or 1
+
+    local fonts = {
+        h1 = love.graphics.newFont(60),
+        h2 = love.graphics.newFont(50),
+        h3 = love.graphics.newFont(40),
+        p = love.graphics.newFont(16),
+    }
+
+    local set_color = function(self, r, g, b)
+        self.colors.r = r
+        self.colors.g = g
+        self.colors.b = b
+    end
+
+    local draw = function(self, tbl_text, index)
+        engine.graphics.setColor(self.colors.r, self.colors.g, self.colors.b)
+        engine.graphics.setFont(fonts[font_size])
+        engine.graphics.printf(self.text, self.x, self.y, wrap_width, align)
+    end
+
+    return {
+        x = x,
+        y = y,
+        text = text,
+        opacity = opacity,
+        set_color = set_color,
+        draw = draw,
+        colors = {
+            r = 1,
+            g = 1,
+            b = 1
+        }
+    }
+end
+
+return Text
