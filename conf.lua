f69f3ef25dfec057b92ca032601c0b6bbec1b51e
paologaleotti <paologaleotti2002@gmail.com>
2023-02-23 19:47:44 +0100

    added conf.lua configuration file

---
+++ conf.lua
@@ -0,0 +1,7 @@
+local engine = require("love")
+
+function engine.conf(app)
+    app.window.width = 1280
+    app.window.height = 720
+    app.window.title = "Asteroids"
+end
--- main.lua
+++ main.lua
@@ -1,22 +1,13 @@
-love = require("love")
+local engine = require("love")
 
-function love.load()
-	player = {
-		x = 20,
-		y = 20
-	}
+function engine.load()
+	engine.mouse.setVisible(false)
 end
 
-function love.update()
-	if love.keyboard.isDown("a") then
-		player.x = player.x - 2
-	end
+function engine.update()
 
-	if love.keyboard.isDown("d") then
-		player.x = player.x + 2
-	end
 end
 
-function love.draw()
-	love.graphics.circle("fill", player.x, player.y, 20)
+function engine.draw()
+
 end
