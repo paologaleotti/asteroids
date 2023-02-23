85cf6f18980ac9731d9055a5f405fd07445dc1e6
paologaleotti <paologaleotti2002@gmail.com>
2023-02-23 20:43:58 +0100

    added Player entity and draw method

---
+++ Player.lua
@@ -0,0 +1,47 @@
+local engine = require("love")
+
+local function calculate_player_vertices(x, y, angle, radius)
+    return x + ((4 / 3) * radius) * math.cos(angle),
+        y - ((4 / 3) * radius) * math.sin(angle),
+        x - radius * (2 / 3 * math.cos(angle) + math.sin(angle)),
+        y + radius * (2 / 3 * math.sin(angle) - math.cos(angle)),
+        x - radius * (2 / 3 * math.cos(angle) - math.sin(angle)),
+        y + radius * (2 / 3 * math.sin(angle) + math.cos(angle))
+end
+
+local function Player(spawn_x, spawn_y)
+    local SHIP_SIZE = 30
+    local VIEW_ANGLE = math.rad(90)
+
+    local thrust = {
+        x = 0,
+        y = 0,
+        speed = 0
+    }
+
+    local draw = function(self)
+        local opacity = 1
+        engine.graphics.setColor(1, 1, 1, opacity)
+        engine.graphics.polygon(
+            "line",
+            calculate_player_vertices(self.x, self.y, self.angle, self.radius)
+        )
+    end
+
+    local move = function(self)
+        local fps = love.timer.getFPS()
+    end
+
+    return {
+        x = spawn_x,
+        y = spawn_y,
+        radius = SHIP_SIZE / 2,
+        angle = VIEW_ANGLE,
+        rotation = 0,
+        thrusting = false,
+        thrust = thrust,
+        draw = draw
+    }
+end
+
+return Player
--- main.lua
+++ main.lua
@@ -1,13 +1,18 @@
 local engine = require("love")
+local Player = require("Player")
+
+
 
 function engine.load()
+	local center_x, center_y = engine.graphics.getWidth() / 2, engine.graphics.getHeight() / 2
 	engine.mouse.setVisible(false)
+
+	player = Player(center_x, center_y)
 end
 
 function engine.update()
-
 end
 
 function engine.draw()
-
+	player:draw()
 end
